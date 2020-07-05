6ce729ee6e89b02dd233a9cc5277d3e8210add3a
Ray8806 <swashouse1@gmail.com>
2020-07-06 01:09:14 +0800

    vision api fix finish

--- vision.js
+++ vision.js
@@ -1,20 +1,27 @@
+const Translate = require('@google-cloud/translate').v2;
+
+const translate = new Translate.Translate({
+    keyFilename: 'ApiKey.json'
+});
+
 async function start(){
     const vision = require('@google-cloud/vision');
 
   // Creates a client
   const client =  new vision.ImageAnnotatorClient({
-      keyFilename: 'visionApi.json'
+      keyFilename: 'ApiKey.json'
   });
 
   // Performs label detection on the image file
-  const [result] =await client.labelDetection('still-image.jpg');
-  const labels = result.labelAnnotations;
-//   console.log('Labels:');
- //labels.forEach(label => console.log(label.description));
-//   document.getElementById('AnsTxt').innerHTML="答案"+labels[1].description;
-    //console.log(labels[1].description);
-    console.log(labels);
-    return labels[0].description;
+    const [result] =await client.labelDetection('still-image.jpg');
+    const labels = result.labelAnnotations;
+    const text = labels[0].description;
+    const target = "zh-TW"
+    let [translations] = await translate.translate(text, target);
+    translations = Array.isArray(translations) ? translations : [translations];
+    console.log('Translations:'+translations);
+    return translations
+    
 }
 start().catch(console.error);
 //console.log(start())
