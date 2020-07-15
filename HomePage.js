6d4cf3183d89aaa9c3b0bd53f4b7501c23b47177
rose_ouo <1314loverose@gmail.com>
2020-07-15 18:16:33 +0800

    Change test1.html button struture and add long click play voice

---
+++ HomePage.js
@@ -0,0 +1,24 @@
+let { ipcRenderer } = require('electron');
+const SSU = new SpeechSynthesisUtterance();
+
+function mouseDown(index) {
+    if(index){
+        // document.getElementById("SpeechToAskBtn").style.color = "red";
+        console.log("index = "+index)
+        console.log("click =? " + index.children[2].childNodes[0].nodeValue)
+        SSU.text = index.children[2].childNodes[0].nodeValue;
+        toggle();
+    }
+  }
+
+//   function mouseUp(index) {
+//     document.getElementById("SpeechToAskBtn").style.color = "green";
+//   }
+
+  function toggle(startOver = true) {
+    speechSynthesis.cancel();
+    if (startOver) {
+        speechSynthesis.voice = "Google 國語（臺灣）";
+        speechSynthesis.speak(SSU);
+    }
+}
