a05f58fa8226528e11e4241011cd01b52156bf9b
kikiouo <kikiouo201@gmail.com>
2020-07-15 20:42:01 +0800

    Add describe game of ㄅ

---
+++ model/game/b.js
@@ -0,0 +1,49 @@
+
+        let isDrawing = false;
+        let x = 0;
+        let y = 0;
+
+        const myPics = document.querySelectorAll('.anvas');
+        console.log('pics'+myPics.length);
+        myPics.forEach((pics)=>{
+            console.log('pics');
+            const context = pics.getContext('2d');
+
+        // event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
+
+        // Add the event listeners for mousedown, mousemove, and mouseup
+        pics.addEventListener('mousedown', e => {
+            x = e.offsetX;
+            y = e.offsetY;
+            isDrawing = true;
+        });
+
+        pics.addEventListener('mousemove', e => {
+            if (isDrawing === true) {
+                drawLine(context, x, y, e.offsetX, e.offsetY);
+                x = e.offsetX;
+                y = e.offsetY;
+            }
+        });
+        window.addEventListener('mouseup', e => {
+            if (isDrawing === true) {
+                drawLine(context, x, y, e.offsetX, e.offsetY);
+                x = 0;
+                y = 0;
+                isDrawing = false;
+            }
+        });
+        });
+        
+
+      
+
+        function drawLine(context, x1, y1, x2, y2) {
+            context.beginPath();
+            context.strokeStyle = 'black';
+            context.lineWidth = 1;
+            context.moveTo(x1, y1);
+            context.lineTo(x2, y2);
+            context.stroke();
+            context.closePath();
+        }
