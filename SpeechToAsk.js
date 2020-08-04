71bf99ee6fb9e8d4c3772575520bebcd4158122e
rose_ouo <1314loverose@gmail.com>
2020-08-05 00:57:24 +0800

    Add picture book link on speech request and fix some position

--- SpeechToAsk.js
+++ SpeechToAsk.js
@@ -46,24 +46,39 @@ if (voiceBtn) {
         const createQA = (text, text2, text3) => `<div class="card text-white mb-3" style="background-color: #92337eba;">
                                         
                                             <div class="card-body" style="margin-top: 30px;">
-                                            <img class="collect_LeftTop" onclick="collect(this)" id="` + CardID_Collect + `" src="icons/bookmark.png"/>
-                                                <div style="float:left; display: block; text-align: left;">
-                                                    <p class="card-title card_Q">Ｑ：</p>
-                                                    <p class="card-title card_Q" style="margin-left: 0px;">${text}</p>
-                                                    <img class="speaker_Q" onclick="speaker(this)" id="` + CardID_Q + `" src="icons/speaker.png" onclick=speakerTest(this)>
-                                                </div>
-                                                <br><br><br><br>
-                                                <div>
-                                                    <img src="https:${text2}">
-                                                </div>
-                                                <br>
-                                                <div style="float:left; display: block; text-align: left;">
-                                                    <p class="card-text card_A" style="float: left;">Ａ：</p>
-                                                    <p class="card-text card_A" style="margin-left: 0px;">${text3}</p>
-                                                    <img class="speaker_A" onclick="speaker(this)" id="` + CardID_A + `" src="icons/speaker.png" />
-                                                </div>
+                                                <img class="collect_LeftTop" onclick="collect(this)" id="` + CardID_Collect + `" src="icons/bookmark.png"/>
+                                                    <div style="float:left; display: block; text-align: left;">
+                                                        <p class="card-title card_Q">Ｑ：</p>
+                                                        <p class="card-title card_Q" style="margin-left: 0px;">${text}</p>
+                                                        <img class="speaker_Q" onclick="speaker(this)" id="` + CardID_Q + `" src="icons/speaker.png" onclick=speakerTest(this)>
+                                                    </div>
+                                                    <br><br><br><br>
+                                                    <div>
+                                                        <img src="https:${text2}">
+                                                    </div>
+                                                    <br>
+                                                    <div style="float:left; display: block; text-align: left;">
+                                                        <p class="card-text card_A" style="float: left;">Ａ：</p>
+                                                        <p class="card-text card_A" style="margin-left: 0px;">${text3}</p>
+                                                        <img class="speaker_A" onclick="speaker(this)" id="` + CardID_A + `" src="icons/speaker.png" />
+                                                    </div>
+
                                             </div>
 
+                                            <div class="card-header contentCss" id="QA_num_" style="background-color: #f8f9fa24; padding-bottom: 40px;">
+                                                <p class="contentlink">相關繪本連結：</p>
+                                                <p class="book_css">蘋果甜蜜蜜</p>
+                                                <img class="speaker_A" onclick="speakerBookName(this)" id="speaker_A" src="icons/speaker.png" style="margin-top: -55px" />
+
+                                                <img src="https://children.moc.gov.tw/resource/animate_image/6892.jpg" style="margin-left: 20px; display: inline;" width="180" height="153" alt="蘋果甜蜜蜜">
+                                                <p style="display: inline; margin-left: 20px; margin-top: 40px; position: absolute; margin-right: 40px;">
+                                                    嫁接的蜜蘋果要先習慣這塊土地，接受泥土的養分之後，才能慢慢慢慢的發芽開花。在這塊土地上接受多元文化洗禮、共同生活的人，不也像蜜蘋果一樣嗎？願藉此，獻上我們最深的祝福！</p>
+                                                <img class="speaker_A" onclick="speakerBook(this)" id="speaker_A" src="icons/speaker.png" style="margin-top: 131px; display: inline; bottom: 31px;" />
+
+                                            </div>
+                                            <div class="card-header" id="QA_num_">
+                                            
+                                            </div>
                                         </div>`;
 
 
@@ -178,6 +193,17 @@ function speakerTest(index) {
 
 function speaker(QA) {
     SSU.text = QA.parentNode.childNodes[3].childNodes[0].nodeValue;
+    toggle();
+}
+
+function speakerBookName(bookName) {
+    SSU.text = bookName.parentNode.childNodes[3].childNodes[0].nodeValue;
+    toggle();
+}
+
+function speakerBook(book) {
+    SSU.text = book.parentNode.childNodes[9].childNodes[0].nodeValue;
+    console.log(SSU.text)
     toggle();
 }
 
