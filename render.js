fd768d33c43940b0f32abf38061582073386fdb7
Ray8806 <swashouse1@gmail.com>
2020-08-09 19:37:54 +0800

    camera explain is finish

--- render.js
+++ render.js
@@ -34,6 +34,7 @@ let identifyBtn = document.querySelector('#identify-js');
 if(identifyBtn){
 identifyBtn.addEventListener('click',()=>{   
     ipcRenderer.send('captrue');
+    
     console.log('ready');
 
     ipcRenderer.on('reply-visionready',(event,data)=>{
@@ -50,6 +51,11 @@ identifyBtn.addEventListener('click',()=>{
 
     ipcRenderer.on('reply-webcrawlerfunction',(event,data) =>{
         if(data==undefined){
+            var ShowVisibility = document.querySelector('.QA_card_area');
+            var ImgVisibility = document.querySelector('img#AnsImg');
+            ShowVisibility.style.visibility = "visible";
+            ImgVisibility.style.visibility = "visible";
+            document.getElementById('leadTxt').innerHTML="辨識成功!!";
             document.getElementById('explainTxt').innerHTML="";
         }else{
             var ShowVisibility = document.querySelector('.QA_card_area');
@@ -67,6 +73,7 @@ identifyBtn.addEventListener('click',()=>{
     ipcRenderer.on('reply-mainjsfunction-captrue',(event,data)=>{
         console.log("hihi");
         document.getElementById("AnsImg").src="./still-image.jpg"
+        document.getElementById('leadTxt').innerHTML="讀取照片。。。";
         ipcRenderer.send('vision');
     })
     
