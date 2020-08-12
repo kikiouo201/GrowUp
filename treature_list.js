02f02425225c2bec189e35f026f7c9fb8e7d2621
rose_ouo <1314loverose@gmail.com>
2020-08-12 21:58:58 +0800

    Add black-overlay when click minMap

---
+++ treature_list.js
@@ -0,0 +1,11 @@
+const minMap = document.querySelector('.minMap');
+
+minMap.addEventListener('click', () => {
+    console.log('minMap onclick');
+
+    const black_overlay = document.querySelector('.black_overlay');
+    black_overlay.style.visibility = "visible";
+
+
+
+});
