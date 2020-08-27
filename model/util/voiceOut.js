cb9d5a8d117d8e4c5875cee03b7465b9bc1936a6
rose_ouo <1314loverose@gmail.com>
2020-08-27 10:55:31 +0800

    Add voice bpm mp3 in drawzhuyin

--- model/util/voiceOut.js
+++ model/util/voiceOut.js
@@ -1,28 +1,34 @@
 const SSU = new SpeechSynthesisUtterance();
 
 function mouseDown(index) {
-    if(index){
+    if (index) {
         // document.getElementById("SpeechToAskBtn").style.color = "red";
-        console.log("index = "+index)
+        console.log("index = " + index)
         console.log("click =? " + index.innerHTML)
         SSU.text = index.children[0].alt;
         toggle();
     }
-  }
+}
 
-  function mouseDownText(text) {
+function mouseDownText(text) {
 
-        console.log("text =? " + text)
-        SSU.text = text;
-        toggle();
+    console.log("text =? " + text)
+    SSU.text = text;
+    toggle();
+    if (text.toString().trim() == 'ㄅ') {
+        console.log("ㄅ")
+        player.play('./TTS/mp3/bpm/b.mp3', function(err) {
+            if (err) throw err
+        })
+    }
 
-  }
+}
 
 //   function mouseUp(index) {
 //     document.getElementById("SpeechToAskBtn").style.color = "green";
 //   }
 
-  function toggle(startOver = true) {
+function toggle(startOver = true) {
     speechSynthesis.cancel();
     if (startOver) {
         speechSynthesis.voice = "Google 國語（臺灣）";
@@ -30,5 +36,5 @@ function mouseDown(index) {
     }
 }
 
-const stopDOM =document.body;
+const stopDOM = document.body;
 stopDOM.addEventListener('dblclick', toggle.bind(null, false));
