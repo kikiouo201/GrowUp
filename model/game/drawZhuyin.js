84079e3457bf804a905f4ca4c94622465dc759b0
kikiouo <kikiouo201@gmail.com>
2020-08-27 11:25:05 +0800

    Refactor moveFinger function

--- model/game/drawZhuyin.js
+++ model/game/drawZhuyin.js
@@ -157,23 +157,24 @@ function moveFinger(){
     }
    
    
-    let drawTime = [1500,2500,3600,4900,6100,7500,9000,11000];
-    let strokeNumber = {b:8,4:8,}
-    
-    setTimeout("document.querySelector('.finger"+id+"').className = 'moveFinger"+id+"1';", drawTime[0]);
-    setTimeout("document.querySelector('.moveFinger"+id+"1').className = 'moveFinger"+id+"2';", drawTime[1]);
-    setTimeout("document.querySelector('.moveFinger"+id+"2').className = 'moveFinger"+id+"3';", drawTime[2]);
-    if(id==4){
-       setTimeout("document.querySelector('.moveFinger"+id+"1').style.visibility='hidden';", 2480);
-       setTimeout("document.querySelector('.moveFinger"+id+"3').style.visibility='visible';", 4800);
-    }
-    setTimeout("document.querySelector('.moveFinger"+id+"3').className = 'moveFinger"+id+"4';", drawTime[3]);
-    setTimeout("document.querySelector('.moveFinger"+id+"4').className = 'moveFinger"+id+"5';", drawTime[4]);
-    setTimeout("document.querySelector('.moveFinger"+id+"5').className = 'moveFinger"+id+"6';", drawTime[5]);
-    setTimeout("document.querySelector('.moveFinger"+id+"6').className = 'moveFinger"+id+"7';", drawTime[6]);
-    setTimeout("document.querySelector('.moveFinger"+id+"7').className = 'moveFinger"+id+"8';", drawTime[7]);
-    setTimeout("document.querySelector('.moveFinger"+id+"8').className = 'finger"+id+"';", 12000);
-    setTimeout("document.querySelector('.finger"+id+"').style.visibility='hidden';", 12050);
+    let drawTime = [1500,2500,3600,4900,6100,7500,9000,10000,11000,12500];
+    let strokeNumber = {b:8,4:8,5:8,6:8,7:6,f1:8}
+        setTimeout("document.querySelector('.finger"+id+"').className = 'moveFinger"+id+"1';", drawTime[0]);
+        console.log(`strokeNumber[id]=${strokeNumber[id]}`);
+        for(let i=1; i<strokeNumber[id];i++){
+            setTimeout("document.querySelector('.moveFinger"+id+i+"').className = 'moveFinger"+id+(i+1)+"';", drawTime[i]);
+            console.log(`moveFinger"+id+(i+1)+=${id+(i+1)}`);
+        }
+        setTimeout("document.querySelector('.moveFinger"+id+(strokeNumber[id])+"').className = 'finger"+id+"';", drawTime[strokeNumber[id]+1]);
+        console.log(`drawTime[strokeNumber[id]+1]=${drawTime[strokeNumber[id]+1]}`)
+        setTimeout("document.querySelector('.finger"+id+"').style.visibility='hidden';", (drawTime[strokeNumber[id]+1]+20));
+
+        if(id==4){
+           setTimeout("document.querySelector('.moveFinger"+id+"1').style.visibility='hidden';", 2480);
+           setTimeout("document.querySelector('.moveFinger"+id+"3').style.visibility='visible';", 4800);
+        }
+
+  
 
 
 }
