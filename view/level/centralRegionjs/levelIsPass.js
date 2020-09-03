315b48afa6d8f670c676cf3294e5292fbea9f59c
kikiouo <kikiouo201@gmail.com>
2020-09-03 10:55:35 +0800

    Add check level pass in hsinchu and Modify checkZhuyin to checkLevelIsPass function name

--- view/level/centralRegionjs/levelIsPass.js
+++ view/level/centralRegionjs/levelIsPass.js
@@ -1,6 +1,6 @@
 let {ipcRenderer }= require('electron');
 
-function checkZhuyin(level_icon,counties) {
+function checkLevelIsPass(level_icon,counties) {
   const level_name = document.querySelectorAll(".level")
   ipcRenderer.send("callZhuyinCondition")
 
@@ -8,11 +8,9 @@ function checkZhuyin(level_icon,counties) {
     // console.log("zhuyin = >"+zhuyin[0])
     console.log("success call reply-callZhuyindata Condition ~~~~ ")
     let levelSize = data.content.length;
-    console.log(`levelSize=${levelSize}`);
+   
     data.content.forEach((level) => {
-        console.log(`level=${JSON.stringify(level.level_name)}`);
         for(let i=0;i<level_name.length;i++){
-            console.log(`level_name[i].getAttribute('alt')=${level_name[i].getAttribute('alt')}`);
             if(level.level_name == level_name[i].getAttribute('alt') ){
                 if(level.level_name !== counties){
                     if(level.ispass == 1 ){
