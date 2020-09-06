1048f7a3edac3d03c183cbc60f1dd6aa05751449
rose_ouo <1314loverose@gmail.com>
2020-09-06 23:54:04 +0800

    Add voice on magic weapon and settimeout event to wait for voice

--- SpeechToAsk.js
+++ SpeechToAsk.js
@@ -332,3 +332,19 @@ function searchImg(keyword) {
 
     })();
 }
+
+function playAudio(name) {
+    var audioCreate = document.createElement("AUDIO");
+    console.log("name:" + name.alt)
+    let id = name.alt;
+    if (audioCreate.canPlayType("audio/mpeg")) {
+        audioCreate.setAttribute("src", `./TTS/mp3/questionMW/${id}.mp3`);
+        console.log(`id:${id}`)
+    }
+
+    // x.setAttribute("controls", "controls");
+    document.body.appendChild(audioCreate);
+    // console.log("OK " + `${id} ` + aaauu.children[0].src)
+    // aaauu.children[0].src = "../../TTS/mp3/bpm/" + `${id}` + ".mp3"
+    audioCreate.play();
+}
