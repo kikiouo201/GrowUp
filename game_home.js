f8f15f718c4a401ddfe314608249637d4171d055
rose_ouo <1314loverose@gmail.com>
2020-09-06 22:35:29 +0800

    Add new voice and add playAudio function on game home

--- game_home.js
+++ game_home.js
@@ -82,3 +82,19 @@ function toggle(startOver = true) {
 
 const stopDOM = document.body;
 stopDOM.addEventListener('dblclick', toggle.bind(null, false));
+
+function playAudio(name) {
+    var audioCreate = document.createElement("AUDIO");
+    console.log("name:" + name.alt)
+    let id = name.alt;
+    if (audioCreate.canPlayType("audio/mpeg")) {
+        audioCreate.setAttribute("src", `./TTS/mp3/${id}.mp3`);
+        console.log(`id:${id}`)
+    }
+
+    // x.setAttribute("controls", "controls");
+    document.body.appendChild(audioCreate);
+    // console.log("OK " + `${id} ` + aaauu.children[0].src)
+    // aaauu.children[0].src = "../../TTS/mp3/bpm/" + `${id}` + ".mp3"
+    audioCreate.play();
+}
