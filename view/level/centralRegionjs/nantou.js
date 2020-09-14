f8190ac065a6b166340aaf172cd6dc1e9b1f7c0e
rose_ouo <1314loverose@gmail.com>
2020-09-14 21:41:20 +0800

    Add nantou voice and click function

--- view/level/centralRegionjs/nantou.js
+++ view/level/centralRegionjs/nantou.js
@@ -1,34 +1,50 @@
 // let {ipcRenderer }= require('electron');
 
 function checkZhuyin() {
-  const zhuyin = document.querySelectorAll("#zhuyin")
-  ipcRenderer.send("callZhuyinCondition")
+    const zhuyin = document.querySelectorAll("#zhuyin")
+    ipcRenderer.send("callZhuyinCondition")
 
-  ipcRenderer.on("reply-callZhuyindata",(event,data) =>{
-    // console.log("zhuyin = >"+zhuyin[0])
-    console.log("success call reply-callZhuyindata Condition ~~~~ ")
-    for( i = 0; i < 9; i++){
-      for( z = 0; z < 9; z++){
+    ipcRenderer.on("reply-callZhuyindata", (event, data) => {
+        // console.log("zhuyin = >"+zhuyin[0])
+        console.log("success call reply-callZhuyindata Condition ~~~~ ")
+        for (i = 0; i < 9; i++) {
+            for (z = 0; z < 9; z++) {
 
-        if(data.content[i].ispass == 1 ){
-          
-          if(data.content[i].level_name == zhuyin[z].getAttribute('alt')){
+                if (data.content[i].ispass == 1) {
 
-            zhuyin[i].src ="../../image/icon/herbal_tea.png"
+                    if (data.content[i].level_name == zhuyin[z].getAttribute('alt')) {
 
-          }
+                        zhuyin[i].src = "../../image/icon/herbal_tea.png"
 
+                    }
+
+                }
+                if (data.content[i].ispass == 0) {
+
+                    if (data.content[i].level_name == zhuyin[z].getAttribute('alt')) {
+
+                        zhuyin[i].src = "../../image/icon/herbal_tea_dark.png"
+
+                    }
+
+                }
+            }
         }
-        if(data.content[i].ispass == 0){
+    })
+}
 
-          if(data.content[i].level_name == zhuyin[z].getAttribute('alt')){
-
-            zhuyin[i].src ="../../image/icon/herbal_tea_dark.png"
-
-          }
-
-        }
-      }  
+function playAudio(name) {
+    var audioCreate = document.createElement("AUDIO");
+    console.log("name:" + name.alt)
+    let id = name.alt;
+    if (audioCreate.canPlayType("audio/mpeg")) {
+        audioCreate.setAttribute("src", `../../TTS/mp3/map/central/${id}.mp3`);
+        console.log(`id:${id}`)
     }
-  })
+
+    // x.setAttribute("controls", "controls");
+    document.body.appendChild(audioCreate);
+    // console.log("OK " + `${id} ` + aaauu.children[0].src)
+    // aaauu.children[0].src = "../../TTS/mp3/bpm/" + `${id}` + ".mp3"
+    audioCreate.play();
 }
