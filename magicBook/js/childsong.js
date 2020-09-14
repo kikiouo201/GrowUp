ab968c3b38722cf999faa0de544116423e95aad5
AppleFinn <pixar99852@gmail.com>
2020-09-14 13:32:26 +0800

    Crawler childsongDate

---
+++ magicBook/js/childsong.js
@@ -0,0 +1,24 @@
+const main = document.querySelector('.maincontent');
+ipcRenderer.on('songCreating', (event, data) => {
+
+  console.log("Success catch Picturebook Data")
+
+  main.innerHTML +=
+  `
+    <div class="song">
+      <div class="songname-div">
+          <h2>${data.name}</h2>
+          <img class="voice" src=" ../icons/speaker.png" />
+      </div>
+      <div class="activity-div">
+          <img src="${data.src}">
+          <button onclick='showWeb("https://children.moc.gov.tw${data.href}")'>
+              <img src="./image/icon_watchVideo.png" />
+              <h2>觀看兒歌</h2>
+          </button>
+      </div>
+    </div>
+  `
+  console.log("Success catch Picturebook Data")
+
+})
