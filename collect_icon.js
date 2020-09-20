bc36f86de00d5a9691395078bcc26d7297e1401d
rose_ouo <1314loverose@gmail.com>
2020-09-20 23:16:17 +0800

    Add play012 function to call mp3

--- collect_icon.js
+++ collect_icon.js
@@ -63,3 +63,19 @@ function playABC(name) {
     // aaauu.children[0].src = "../../TTS/mp3/bpm/" + `${id}` + ".mp3"
     audioCreate.play();
 }
+
+function play012(name) {
+    var audioCreate = document.createElement("AUDIO");
+    console.log("name:" + name.alt)
+    let id = name.alt;
+    if (audioCreate.canPlayType("audio/mpeg")) {
+        audioCreate.setAttribute("src", `./TTS/mp3/ABC/${id}.mp3`);
+        console.log(`id:${id}`)
+    }
+
+    // x.setAttribute("controls", "controls");
+    document.body.appendChild(audioCreate);
+    // console.log("OK " + `${id} ` + aaauu.children[0].src)
+    // aaauu.children[0].src = "../../TTS/mp3/bpm/" + `${id}` + ".mp3"
+    audioCreate.play();
+}
