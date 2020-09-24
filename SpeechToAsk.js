621b5c46d3769a1174b164c88b4aafa53e3478cf
rose_ouo <1314loverose@gmail.com>
2020-09-24 22:24:24 +0800

    Add play magicWeapon voice function

--- SpeechToAsk.js
+++ SpeechToAsk.js
@@ -377,6 +377,22 @@ function playAudio(name) {
     audioCreate.play();
 }
 
+function playVoiceAndCapture(name) {
+    var audioCreate = document.createElement("AUDIO");
+    // console.log("name:" + name.alt)
+    let id = name.alt;
+    if (audioCreate.canPlayType("audio/mpeg")) {
+        audioCreate.setAttribute("src", `./TTS/mp3/questionMW/${id}.mp3`);
+        // console.log(`id:${id}`)
+    }
+
+    // x.setAttribute("controls", "controls");
+    document.body.appendChild(audioCreate);
+    // console.log("OK " + `${id} ` + aaauu.children[0].src)
+    // aaauu.children[0].src = "../../TTS/mp3/bpm/" + `${id}` + ".mp3"
+    audioCreate.play();
+}
+
 function onImageLoaded(url, cb) {
     var image = new Image()
     image.src = url
