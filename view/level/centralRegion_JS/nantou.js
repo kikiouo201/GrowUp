06e57f827abc94c19489d24b0f7461b0fa75f537
AppleFinn <pixar99852@gmail.com>
2020-09-27 20:32:50 +0800

    Finish nantou

--- view/level/centralRegion_JS/nantou.js
+++ view/level/centralRegion_JS/nantou.js
@@ -32,3 +32,29 @@ function checkZhuyin() {
         }
     })
 }
+
+
+function changeInfor(type) {
+    const picture = document.querySelector("#picture");
+    const speaker = document.querySelector("#speaker");
+    const intro = document.querySelector("#intro");
+    document.querySelector(".fullContent").style.display='block'
+
+    switch (type) {
+        case 0:
+            picture.src= "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/%E5%8D%97%E6%8A%95%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83.png/350px-%E5%8D%97%E6%8A%95%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83.png";
+            intro.textContent = "南投地區，是唯一不靠海的縣市，而南投的地名由來，是因為平埔族阿立昆族的族語「Ramtau」，臺語就是音譯為「南投」。"
+            speaker.alt = "nantouRegion"
+            break;
+        case 1:
+            picture.src = "https://s.yimg.com/os/zh-Hant-TW/homerun/tw-travel.travelweb.com.tw/71e4ae358ca78944aaec7b6a4320bd07";
+            intro.textContent = "高山茶，是指在海拔1000公尺以上茶園，所摘取製作的茶葉。"
+            speaker.alt = "nantou_f1"
+            break;
+        case 2:
+            picture.src = "https://www.funtime.com.tw/blog/wp-content/uploads/2020/04/25.jpg";
+            intro.textContent = "日月潭，東邊的形狀像「日」，而西邊的形狀像「月」，所以稱做日月潭，是一座美麗的高山湖泊。"
+            speaker.alt = "nantou_f2"
+            break;
+    }
+}
