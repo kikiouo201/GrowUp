48cc6ede0665334fcb912dd28f20996a55bd06fb
AppleFinn <pixar99852@gmail.com>
2020-10-06 21:11:28 +0800

    Add crawler loading

--- magicBook/js/crawler.js
+++ magicBook/js/crawler.js
@@ -1,11 +1,12 @@
 const {ipcRenderer }= require('electron');
 
-function showWeb(url){
-  console.log('ShowWeb');
+function showWeb(url,ele){
+  console.log('ShowWeb&Ele =>' +ele);
+  ele.style.display='block'
+
   ipcRenderer.send('crawlerShowWeb',url);
 }
 function songGetDate(){
   ipcRenderer.send('crawlerGetDate');
   console.log('SongData Geting');
-
 }
