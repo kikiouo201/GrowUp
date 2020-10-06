8f54b0ef9080eb6c45de15bfd182ceee7ea563f0
AppleFinn <pixar99852@gmail.com>
2020-10-06 21:52:41 +0800

    Add loading colse function

--- magicBook/js/crawler.js
+++ magicBook/js/crawler.js
@@ -1,12 +1,20 @@
 const {ipcRenderer }= require('electron');
+let full ;
 
 function showWeb(url,ele){
   console.log('ShowWeb&Ele =>' +ele);
+  full = ele;
   ele.style.display='block'
 
   ipcRenderer.send('crawlerShowWeb',url);
 }
+
 function songGetDate(){
   ipcRenderer.send('crawlerGetDate');
   console.log('SongData Geting');
 }
+
+ipcRenderer.on('colseLoading', (event, url) => {
+  console.log("close LodaingView")
+  full.style.display='none'
+})
