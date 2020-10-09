25584c582d7c13f1023d06c4083e9acc84f640f2
rose_ouo <1314loverose@gmail.com>
2020-10-09 17:52:59 +0800

    Add voice function in camera.js

--- camera.js
+++ camera.js
@@ -1,3 +1,14 @@
 function open_mjpgstreamer() {
-  ipcRenderer.send('open-mjpg-streamer')
+    ipcRenderer.send('open-mjpg-streamer')
+}
+
+function playVoiceAndCapture(name) {
+    var audioCreate = document.getElementById("AUDIO");
+    // console.log("name:" + name.alt)
+    let id = name.alt;
+    if (audioCreate.canPlayType("audio/mpeg")) {
+        audioCreate.setAttribute("src", `./TTS/mp3/questionMW/${id}.mp3`);
+        // console.log(`id:${id}`)
+    }
+    audioCreate.play();
 }
