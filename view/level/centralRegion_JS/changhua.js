037f017a6cd15d272845134f90539b4040fc2093
Ray8806 <swashouse1@gmail.com>
2020-10-09 12:56:03 +0800

    centralRegion js fix

--- view/level/centralRegion_JS/changhua.js
+++ view/level/centralRegion_JS/changhua.js
@@ -1,34 +1,25 @@
-let { ipcRenderer } = require('electron');
-
-function checkZhuyin() {
-    const zhuyin = document.querySelectorAll("#zhuyin")
-    ipcRenderer.send("callZhuyinCondition")
-
-    ipcRenderer.on("reply-callZhuyindata", (event, data) => {
-        // console.log("zhuyin = >"+zhuyin[0])
-        console.log("success call reply-callZhuyindata Condition ~~~~ ")
-        for (i = 16; i < 23; i++) {
-            for (z = 0; z < 7; z++) {
-
-                if (data.content[i].ispass == 1) {
-
-                    if (data.content[i].level_name == zhuyin[z].getAttribute('alt')) {
-
-                        zhuyin[z].src = "../../image/icon/meat_ball.png"
-
-                    }
-
-                }
-                if (data.content[i].ispass == 0) {
-
-                    if (data.content[i].level_name == zhuyin[z].getAttribute('alt')) {
-
-                        zhuyin[z].src = "../../image/icon/meat_ball_dark.png"
-
-                    }
-
-                }
-            }
-        }
-    })
+function changeInfor(type) {
+    const picture = document.querySelector("#picture");
+    const speaker = document.querySelector("#speaker");
+    const intro = document.querySelector("#intro");
+    document.querySelector(".fullContent").style.display='block'
+  
+    switch (type) {
+        case 0:
+            picture.src= "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/%E5%8D%97%E6%8A%95%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83.png/350px-%E5%8D%97%E6%8A%95%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83.png";
+            intro.textContent = "土地肥沃，物產豐饒，有「臺灣穀倉」之稱。早年鹿港發展成為中部地區最具發展潛力的城鎮。"
+            speaker.alt = "changhuaRegion"
+            break;
+        case 1:
+            picture.src = "https://tourism.chcg.gov.tw/upload/27/2017081612043794500.jpg";
+            intro.textContent = "八卦山位於彰化市東方邊的山。交通部觀光局將獅頭山、梨山、八卦山 3 個風景區合併為「參山國家風景區」。"
+            speaker.alt = "changhua_f1"
+            break;
+        case 2:
+            picture.src = "https://leafyeh.com/wp-content/uploads/flickr/48250627257_98f549c20e_o.jpg";
+            intro.textContent = "這些發電機位於臺灣彰化縣芳苑鄉。通過環評審查，運轉後將可為台灣280萬個家庭供電。"
+            speaker.alt = "changhua_f2"
+            break;
+       
+  }
 }
--- view/level/centralRegion_JS/taichung.js
+++ view/level/centralRegion_JS/taichung.js
@@ -1,34 +1,30 @@
-let { ipcRenderer } = require('electron');
-
-function checkZhuyin() {
-    const zhuyin = document.querySelectorAll("#zhuyin")
-    ipcRenderer.send("callZhuyinCondition")
-
-    ipcRenderer.on("reply-callZhuyindata", (event, data) => {
-        // console.log("zhuyin = >"+zhuyin[0])
-        console.log("success call reply-callZhuyindata Condition ~~~~ ")
-        for (i = 23; i < 30; i++) {
-            for (z = 0; z < 7; z++) {
-
-                if (data.content[i].ispass == 1) {
-
-                    if (data.content[i].level_name == zhuyin[z].getAttribute('alt')) {
-
-                        zhuyin[z].src = "../../image/icon/suncake.png"
-
-                    }
-
-                }
-                if (data.content[i].ispass == 0) {
-
-                    if (data.content[i].level_name == zhuyin[z].getAttribute('alt')) {
-
-                        zhuyin[z].src = "../../image/icon/suncake_dark.png"
-
-                    }
-
-                }
-            }
-        }
-    })
+function changeInfor(type) {
+    const picture = document.querySelector("#picture");
+    const speaker = document.querySelector("#speaker");
+    const intro = document.querySelector("#intro");
+    document.querySelector(".fullContent").style.display='block'
+  
+    switch (type) {
+        case 0:
+            picture.src= "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/%E5%8D%97%E6%8A%95%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83.png/350px-%E5%8D%97%E6%8A%95%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83.png";
+            intro.textContent = "六都之一，是臺灣西半部的樞紐，四季氣候宜人。日治時期，臺中遂躍身為中臺灣政治、經濟、交通、文化的重鎮。"
+            speaker.alt = "taichungRegion"
+            break;
+        case 1:
+            picture.src = "http://www.ifhouse.com.tw/upload_file/ifhouse/901/15299979011.jpg";
+            intro.textContent = "一種甜餡薄餅，通常內餡是麥芽糖，中臺灣的名產之一。"
+            speaker.alt = "taichung_f1"
+            break;
+        case 2:
+            picture.src = "https://www.taiwan.net.tw/pic.ashx?qp=1/big_scenic_spots/pic_9547_18.jpg&sizetype=3";
+            intro.textContent = "武陵農場位於臺中市北橫宜蘭支線。農場內阡陌縱橫，種植高山蔬果與茶葉，四季風情不同。"
+            speaker.alt = "taichung_f2"
+            break;
+        case 3:
+            picture.src = "https://travel.taichung.gov.tw/Utility/DisplayImage?id=24379";
+            intro.textContent = "日本建築師伊東豊雄設計，以「美聲涵洞」為概念，採用曲牆、孔洞與管狀等等設計，建構一棟沒有樑柱支撐。"
+            speaker.alt = "taichung_f3"
+            break;
+       
+  }
 }
