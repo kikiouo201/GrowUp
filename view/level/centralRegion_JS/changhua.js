2219d87cf8c13c5133057526f187e138f237c6de
weisiou <880702angel@gmail.com>
2020-10-13 19:22:53 +0800

    change central city scence introduce

--- view/level/centralRegion_JS/changhua.js
+++ view/level/centralRegion_JS/changhua.js
@@ -6,19 +6,24 @@ function changeInfor(type) {
   
     switch (type) {
         case 0:
-            picture.src= "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/%E5%8D%97%E6%8A%95%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83.png/350px-%E5%8D%97%E6%8A%95%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83.png";
-            intro.textContent = "土地肥沃，物產豐饒，有「臺灣穀倉」之稱。早年鹿港發展成為中部地區最具發展潛力的城鎮。"
+            picture.src= "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/%E5%BD%B0%E5%8C%96%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83.png/350px-%E5%BD%B0%E5%8C%96%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83.png";
+            intro.textContent = "位於臺灣西部，古稱半線。土地肥沃，物產豐饒，有「臺灣穀倉」之稱。早年鹿港發展成為中部地區最具發展潛力的城鎮。"
             speaker.alt = "changhuaRegion"
             break;
         case 1:
-            picture.src = "https://tourism.chcg.gov.tw/upload/27/2017081612043794500.jpg";
-            intro.textContent = "八卦山位於彰化市東方邊的山。交通部觀光局將獅頭山、梨山、八卦山 3 個風景區合併為「參山國家風景區」。"
+            picture.src = "https://imageproxy.icook.network/resize?height=600&nocrop=false&stripmeta=true&type=auto&url=http%3A%2F%2Ftokyo-kitchen.icook.tw.s3.amazonaws.com%2Fuploads%2Frecipe%2Fcover%2F193513%2F4f4604bd32819a5a.jpg&width=800";
+            intro.textContent = "彰化特色小吃。彰化以北多用油炸油泡、彰化以南多為炊蒸。而彰化肉圓還有將它製成涼式的吃法，將蒸好的肉圓放冷後，再放到冰涼的湯水內，夏天吃極為清爽。"
             speaker.alt = "changhua_f1"
             break;
         case 2:
-            picture.src = "https://leafyeh.com/wp-content/uploads/flickr/48250627257_98f549c20e_o.jpg";
-            intro.textContent = "這些發電機位於臺灣彰化縣芳苑鄉。通過環評審查，運轉後將可為台灣280萬個家庭供電。"
+            picture.src = "https://tourism.chcg.gov.tw/upload/27/2017081612043794500.jpg";
+            intro.textContent = "有釋迦牟尼大佛像一尊。登臨其上，可俯瞰彰化市及鄰近農區，是彰化有名的風景區。"
             speaker.alt = "changhua_f2"
+            break;
+        case 3:
+            picture.src = "https://leafyeh.com/wp-content/uploads/flickr/48250627257_98f549c20e_o.jpg";
+            intro.textContent = "有著外星人的秘密基地與外星人打蛋器之稱，主要是蓄電用的風車，形狀為三葉式造型，是目前全世界唯一獲得認證的垂直軸小風。"
+            speaker.alt = "changhua_f3"
             break;
        
   }
--- view/level/centralRegion_JS/yunlin.js
+++ view/level/centralRegion_JS/yunlin.js
@@ -43,27 +43,27 @@ function changeInfor(type) {
     switch (type) {
         case 0:
             picture.src = "https://www.ylhpb.gov.tw/df_ufiles/df_pics/Images/map_ch.png"
-            intro.textContent = "雲林地區，位在臺灣西方的中南部，地形平坦，氣候溫和。雲林帶給人們的印象通常為「臺灣的糧倉」。"
+            intro.textContent = "位於臺灣中部，地形平坦，氣候溫和。主要農產品有稻米、甘蔗、甘藷、玉蜀黍、落花生等，因此雲林帶給人們的印象通常為「臺灣的糧倉」。"
             speaker.alt = "yunlinRegion"
             break;
         case 1:
             picture.src = "http://4.bp.blogspot.com/-wCqi3g4nycM/UlwFKUJ5DRI/AAAAAAAAJyk/U4QGoS8a1BM/s1600/DSC00309.JPG";
-            intro.textContent = "西螺醬油是雲林重要的特產，因為水質、溫度和濕度的合適，雲林西螺號稱臺灣的「醬油王國」。"
+            intro.textContent = "是雲林重要的特產，因為水質、溫度和濕度的合適，雲林西螺號稱臺灣的「醬油王國」。"
             speaker.alt = "yunlin_f1"
             break;
         case 2:
             picture.src = "http://fancyworld.janfusun.com.tw/images/about/about_img01.jpg";
-            intro.textContent = "劍湖山世界主題樂園是臺灣最有人氣的主題樂園之一，有著最新鮮的玩樂設施，綜合「休閒、遊樂、文化、科技」四大功能，讓參與的遊客不由得大聲高喊！"
+            intro.textContent = "是臺灣最有人氣的主題樂園之一，有著最新鮮的玩樂設施，綜合「休閒、遊樂、文化、科技」四大功能，讓參與的遊客不由得大聲高喊！"
             speaker.alt = "yunlin_f2"
             break;
         case 3:
             picture.src = "https://tour.yunlin.gov.tw/upload/shopInfo/20180911112832.jpg";
-            intro.textContent = "北港朝天宮俗稱北港媽祖廟。北港朝天宮終年香火鼎盛，每年最熱鬧的祭典分別是元宵節和媽祖誕辰。"
+            intro.textContent = "俗稱北港媽祖廟。北港朝天宮終年香火鼎盛，每年最熱鬧的祭典分別是元宵節和媽祖誕辰。"
             speaker.alt = "yunlin_f3"
             break;
         case 4:
             picture.src = "https://cdn.walkerland.com.tw/images/upload/poi/p90461/m36804/c49311271890bf8af8237e7c901d63b42c710866.jpg";
-            intro.textContent = "金億陽蘑菇農場有2000捆的稻草捲，每綑稻草重達350斤(210公斤)，堆積成金字塔型的稻草山。"
+            intro.textContent = "有2000捆的稻草捲，每綑稻草重達210公斤，堆積成金字塔型的稻草山。"
             speaker.alt = "yunlin_f4"
             break;
     }
