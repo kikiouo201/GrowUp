c28437b68349d21ec2c53343f3a0703fdc8eabe4
rose_ouo <1314loverose@gmail.com>
2020-10-19 22:13:26 +0800

    Modify voiceOut on checkAllLevelIsPass js

--- model/util/checkAllLevelIsPass.js
+++ model/util/checkAllLevelIsPass.js
@@ -1,5 +1,5 @@
 let { ipcRenderer: ipcRenderer2 } = require('electron');
-let voiceOut = require('../../../GrowUp/model/util/voiceOut.js');
+let voiceOut = require('../../model/util/voiceOut.js');
 
 let levelNameConversion = {
     'ㄅ': 'b',
