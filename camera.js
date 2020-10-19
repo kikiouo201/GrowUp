1bf9b8d6a8690cc1d6444ce41a5cb8b8ffa98d77
AppleFinn <pixar99852@gmail.com>
2020-10-19 16:59:21 +0800

    Reset magic_weapon button voice

--- camera.js
+++ camera.js
@@ -7,9 +7,9 @@ function open_mjpgstreamer() {
 function playVoiceAndCapture(name) {
     var audioCreate = document.getElementById("AUDIO");
     // console.log("name:" + name.alt)
-    let id = name.alt;
+
     if (audioCreate.canPlayType("audio/mpeg")) {
-        audioCreate.setAttribute("src", `./TTS/mp3/questionMW/${id}.mp3`);
+        audioCreate.setAttribute("src", `./TTS/mp3/questionMW/${name}.mp3`);
         // console.log(`id:${id}`)
     }
     audioCreate.play();
