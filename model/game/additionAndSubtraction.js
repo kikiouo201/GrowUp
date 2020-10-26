934f4918d877f32ffc670541ca2613d116bb77c9
kikiouo <kikiouo201@gmail.com>
2020-10-26 17:12:51 +0800

    Modify additionAndSubtraction interface

--- model/game/additionAndSubtraction.js
+++ model/game/additionAndSubtraction.js
@@ -1,10 +1,33 @@
 let { ipcRenderer:ipcRenderer2  } = require('electron');
 function judgment(number,levelId){
+
+  const status = document.querySelector('.status');
+  const props = document.querySelector('.props');
+
     if(number==5){
-        alert("答對喔!");
+        
+      status.innerHTML = '<img src="../../image/drawZhuyin/good.png" width="200px"/>你好棒';
+      props.style.visibility = "visible";
+      
+
         ipcRenderer2.send("levelIsPass", levelId);
     }
     else{
-      alert("不對喔!");
+      status.innerHTML = '<img src="../../image/drawZhuyin/tryAgain.png" width="200px"/>失敗';
+      props.style.visibility = "hidden";
+      //alert("不對喔!");
     }
+    const black_overlay = document.querySelector('.black_overlay');
+    black_overlay.style.visibility = "visible";
   }
+
+  function challengeAgain() {
+    let counties = (new URLSearchParams(location.search)).get("counties");
+    window.location.href = `../../view/game/additionAndSubtraction.html?id=${levelId}`;
+}
+
+
+
+
+
+
