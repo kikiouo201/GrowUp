6df37dd50bd4b7958c81d3ffd61a6425142fc22d
rose_ouo <1314loverose@gmail.com>
2020-10-26 23:04:41 +0800

    Add yunlin voice and add title name

--- view/level/centralRegion_JS/yunlin.js
+++ view/level/centralRegion_JS/yunlin.js
@@ -38,6 +38,7 @@ function changeInfor(type) {
     const picture = document.querySelector("#picture");
     const speaker = document.querySelector("#speaker");
     const intro = document.querySelector("#intro");
+    const title = document.querySelector('#title');
     document.querySelector(".fullContent").style.display = 'block'
 
     switch (type) {
@@ -45,26 +46,41 @@ function changeInfor(type) {
             picture.src = "https://www.ylhpb.gov.tw/df_ufiles/df_pics/Images/map_ch.png"
             intro.textContent = "位於臺灣中部，地形平坦，氣候溫和。主要農產品有稻米、甘蔗、甘藷、玉蜀黍、落花生等，因此雲林帶給人們的印象通常為「臺灣的糧倉」。"
             speaker.alt = "yunlinRegion"
+            title.textContent = "雲林地區";
+            title.style.left = "32.5%"
+            title.style.width = "220px"
             break;
         case 1:
             picture.src = "http://4.bp.blogspot.com/-wCqi3g4nycM/UlwFKUJ5DRI/AAAAAAAAJyk/U4QGoS8a1BM/s1600/DSC00309.JPG";
             intro.textContent = "是雲林重要的特產，因為水質、溫度和濕度的合適，雲林西螺號稱臺灣的「醬油王國」。"
             speaker.alt = "yunlin_f1"
+            title.textContent = "西螺醬油";
+            title.style.left = "32.5%"
+            title.style.width = "220px"
             break;
         case 2:
             picture.src = "http://fancyworld.janfusun.com.tw/images/about/about_img01.jpg";
             intro.textContent = "是臺灣最有人氣的主題樂園之一，有著最新鮮的玩樂設施，綜合「休閒、遊樂、文化、科技」四大功能，讓參與的遊客不由得大聲高喊！"
             speaker.alt = "yunlin_f2"
+            title.textContent = "劍湖山世界";
+            title.style.left = "32.5%"
+            title.style.width = "220px"
             break;
         case 3:
             picture.src = "https://tour.yunlin.gov.tw/upload/shopInfo/20180911112832.jpg";
             intro.textContent = "俗稱北港媽祖廟。北港朝天宮終年香火鼎盛，每年最熱鬧的祭典分別是元宵節和媽祖誕辰。"
             speaker.alt = "yunlin_f3"
+            title.textContent = "北港朝天宮";
+            title.style.left = "32.5%"
+            title.style.width = "220px"
             break;
         case 4:
             picture.src = "https://cdn.walkerland.com.tw/images/upload/poi/p90461/m36804/c49311271890bf8af8237e7c901d63b42c710866.jpg";
-            intro.textContent = "有2000捆的稻草捲，每綑稻草重達210公斤，堆積成金字塔型的稻草山。"
+            intro.textContent = "農場有2000捆的稻草捲，每綑稻草重達210公斤，堆積成金字塔型的稻草山。"
             speaker.alt = "yunlin_f4"
+            title.textContent = "金億陽蘑菇農場";
+            title.style.left = "25%"
+            title.style.width = "320px"
             break;
     }
 }
