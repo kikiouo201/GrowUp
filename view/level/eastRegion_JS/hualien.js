042e0a9622fa91e3d35bf7e665607feaf8e7fad6
Ray8806 <swashouse1@gmail.com>
2020-10-29 00:43:21 +0800

    add east city scene introduce title

--- view/level/eastRegion_JS/hualien.js
+++ view/level/eastRegion_JS/hualien.js
@@ -9,21 +9,33 @@ function changeInfor(type) {
             picture.src = "https://house.netete.com/images/hualienmap.jpg";
             intro.textContent = "位於臺灣本島東部。是臺灣面積最大的縣級行政區。花蓮地區是台灣原住民最多的區域，其中以第一大族阿美族分布最廣。"
             speaker.alt = "taitungRegion"
+            title.textContent = "花蓮地區";
+            title.style.left = "33%"
+            title.style.width = "220px"
             break;
         case 1:
             picture.src = "https://www.ner.gov.tw/api/images/5affbec7581023000596746f/2400/jpeg";
             intro.textContent = "栽種品種主要為「華寶西瓜」，產量是全台第一，且富含養分，讓西瓜的口感「沙甜」且「清脆」!"
             speaker.alt = "taitung_f1"
+            title.textContent = "花蓮大西瓜";
+            title.style.left = "33%"
+            title.style.width = "220px"
             break;
         case 2:
             picture.src = "https://s.yimg.com/zp/MerchandiseSpec/99DE9C85C0-SP-6215750.jpg";
             intro.textContent = "俗稱花蓮海洋公園。是臺灣第一座以海洋為主題，結合了自然公園與休閒飯店的主題樂園。"
             speaker.alt = "taitung_f2"
+            title.textContent = "遠雄海洋公園";
+            title.style.left = "33%"
+            title.style.width = "220px"
             break;
         case 3:
             picture.src = "https://www.erv-nsa.gov.tw/image/10343/1024x768";
             intro.textContent = "是一座開放式的休閒牧場，能看見成群的乳牛或鴕鳥悠閒漫步在草地上。以放牧乳牛、生產各式鮮奶產品而聞名。"
             speaker.alt = "taitung_f3"
+            title.textContent = "瑞穗牧場";
+            title.style.left = "33%"
+            title.style.width = "220px"
             break;
 
     }
--- view/level/eastRegion_JS/taitung.js
+++ view/level/eastRegion_JS/taitung.js
@@ -9,26 +9,41 @@ function changeInfor(type) {
           picture.src= "../../image/map/Taitung_introduce.png";
           intro.textContent = "位於臺灣東南部。擁有豐富的生態資源。海岸、高山、森林、溪谷都保持著自然完整的風貌，處處都是天然美景。"
           speaker.alt = "taitungRegion"
+          title.textContent = "臺東地區";
+          title.style.left = "33%"
+          title.style.width = "220px"
           break;
       case 1:
           picture.src = "https://i2.wp.com/www.zztaitung.com/wp-content/uploads/2019/06/2019%E5%8F%B0%E6%9D%B1%E7%86%B1%E6%B0%A3%E7%90%83%E6%97%85%E9%81%8A%E5%98%89%E5%B9%B4%E8%8F%AF-17.jpg?resize=900%2C600";
           intro.textContent = "是將氣球內的氣體用瓦斯加熱，使氣球往高空飄浮。每年6月至8月所舉辦的臺灣國際熱氣球嘉年華，可以坐在熱氣球上觀賞花東縱谷的美景。"
           speaker.alt = "taitung_f1"
+          title.textContent = "熱氣球";
+          title.style.left = "33%"
+          title.style.width = "220px"
           break;
       case 2:
           picture.src = "https://www.erv-nsa.gov.tw/image/10397/1024x768";
           intro.textContent = "是全台唯一的坡地牧場。從事森林浴、滾草、餵食黑白相間的荷蘭種乳牛等觀光活動，以出產新鮮、香醇的初鹿鮮奶聞名。"
           speaker.alt = "taitung_f2"
+          title.textContent = "初鹿牧場";
+          title.style.left = "33%"
+          title.style.width = "220px"
           break;
       case 3:
           picture.src = "https://tour.taitung.gov.tw/image/753/1024x768";
           intro.textContent = "俗稱卑南山。因外形與鯉魚相似而得名，是臺東市區最寬廣的休閒綠地也是最明顯的地標和風景區之一。"
           speaker.alt = "taitung_f3"
+          title.textContent = "鯉魚山";
+          title.style.left = "33%"
+          title.style.width = "220px"
           break;
       case 4:
           picture.src = "https://tour.taitung.gov.tw/image/23686/1024x768";
           intro.textContent = "是伯朗大道上的一棵茄苳樹，枝葉茂密、適合乘涼，周圍為一望無際的稻田，這顆樹孤立其中，配上遠方的山景，有著拍照的好景色。"
           speaker.alt = "taitung_f4"
+          title.textContent = "金城武樹";
+          title.style.left = "33%"
+          title.style.width = "220px"
           break;
            
   }
