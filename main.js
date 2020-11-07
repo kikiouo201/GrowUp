7c2333ac9b8a59dd00ce8a5dceffe67d7a208dde
AppleFinn <pixar99852@gmail.com>
2020-11-07 13:06:37 +0800

    Change isNetwork to false

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ var player = require('play-sound')(opts = {})
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win = null;
-let IsNetwork = true;
+let IsNetwork = false;
 let askhomework = false;
 let camehomework = false;
 //true 有網路
